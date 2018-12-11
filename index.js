080c7557543abbaa323f716b1120079e4f82a35c
Roy <Roy_Chun@ultimatesoftware.com>
2018-12-11 15:21:02 -0500

    added mongo dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 console.log(process.env.VCAP_SERVICES);
-const url = process.env.VCAP_SERVICES['ucp-mongo-ssl'][0].credentials.uri;
+const url = process.env.VCAP_SERVICES["ucp-mongo-ssl"][0].credentials.uri;
 
 console.log(url);
 
